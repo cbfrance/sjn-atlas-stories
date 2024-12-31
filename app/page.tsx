15b062b3539d2532de8746ba0430efa04f3ba7a2
Chris B. France <7366+cbfrance@users.noreply.github.com>
2024-12-31 11:16:15 -0800

    fix: escape quotes in page.tsx for build

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
       <h1>Solutions Universe</h1>
       <Scrollyteller map="christopherbfrance/articles-map-20241230-222047/map/2e4eca5c-e55c-4c11-9b31-c600c7b2b5e5">
         <Folio zoom={{ x: [-50, 50], y: [-50, 50] }}>
-          This is a map of all of the articles accepted into the Story Tracker database from early 2022 to late 2024. Each region is a "galaxy" that roughly clusters articles by topic.
+          This is a map of all of the articles accepted into the Story Tracker database from early 2022 to late 2024. Each region is a &ldquo;galaxy&rdquo; that roughly clusters articles by topic.
           <small>For the 2024 Story Tracker map, we used the <span className="font-mono"><a href="https://arxiv.org/abs/2402.01613">Nomic Embed 1.5</a></span> model to process embeddings.</small>
         </Folio>
         <Folio hash="8hqa">
